dcbf6f4b4e20bcc6eee98702593f36f0a8601a4b
Bryium <bryiumonyancha@gmail.com>
2024-10-09 12:46:53 +0300

    change app.js

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View, Text } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>
-        Hello World ...I love my sister so much bro You can't tell me anything
-        man
-      </Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Current Weather</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
