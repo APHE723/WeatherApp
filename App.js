edcb86b87e32db3a7195528bff57916a6b62b7ee
Bryium <bryiumonyancha@gmail.com>
2024-10-09 12:58:28 +0300

    add safeAreaContext

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 const App = () => {
   return (
-    <View>
-      <Text>Current Weather</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>
+          My name is Bryium Onyancha. I work in google Kenya as a software
+          engineer. happy.{" "}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
