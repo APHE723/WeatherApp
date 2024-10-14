0d3af374474fb93c1ca6227f7e1b57060b90fd1d
Bryium <bryiumonyancha@gmail.com>
2024-10-14 12:28:02 +0300

    add new text and styled them

--- App.js
+++ App.js
@@ -6,7 +6,12 @@ const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.textContainer}>
-        <Text>Current Weather. </Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8 </Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -21,6 +26,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "pink",
     padding: 10,
+    alignItems: "center",
+  },
+  temp: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    fontSize: 30,
+    color: "black",
+  },
+  highLow: {
+    fontSize: 20,
+    color: "black",
+  },
+  highLowWrapper: {
+    flexDirection: "row",
   },
 });
 
