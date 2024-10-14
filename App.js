3998e5ce3e8cf6d8932ef85928379f6e76c7d971
Bryium <bryiumonyancha@gmail.com>
2024-10-14 11:50:59 +0300

    change babel file and background color to pink

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.textContainer}>
         <Text>
-          My name is Bryium Onyancha. I work in google Kenya as a software
-          engineer. happy.{" "}
+          My name is Bryium Onyancha. I work in Google Kenya as a software
+          engineer. Happy.{" "}
         </Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  textContainer: {
+    backgroundColor: "pink",
+    padding: 10,
+  },
+});
 
 export default App;
