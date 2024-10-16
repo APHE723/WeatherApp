a80e9f890435b69442c542cc48b737e8378206a6
Bryium <bryiumonyancha@gmail.com>
2024-10-16 19:24:51 +0300

    add the array and rendered it

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+const DATA = [
+  {},
+  {},
+  {},
+]
 const UpcomingWeather = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Upcoming Weather</Text>
+      <FlatList 
+      data= {}
+      renderItem={}
+      />
     </SafeAreaView>
   );
 };
