2c2277c678b4f1af3a71a9c403f580b615c525d3
Bryium <bryiumonyancha@gmail.com>
2024-10-26 15:38:02 +0300

    add statusbar and background image

--- src/screens/City.js
+++ src/screens/City.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, ImageBackground, StatusBar } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const City = () => {
   return (
-    <SafeAreaView style={styles.conatiner}>
-      <Text>City</Text>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        // eslint-disable-next-line no-undef
+        source={require("../../assets/City.png")}
+        style={styles.imageLayout}
+      ></ImageBackground>
     </SafeAreaView>
   );
 };
@@ -13,6 +17,11 @@ const City = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  imageLayout: {
+    flex: 1,
   },
 });
+
 export default City;
