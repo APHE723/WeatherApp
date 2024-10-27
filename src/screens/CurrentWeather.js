d240dcc021baa132e24802f18775c160c03e97fc
Bryium <bryiumonyancha@gmail.com>
2024-10-27 17:38:25 +0300

    added props to currentweather file

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import RowText from "../components/RowText";
 
 const CurrentWeather = () => {
   return (
@@ -10,15 +11,24 @@ const CurrentWeather = () => {
         <Ionicons name="sunny-outline" size={100} color="black" />
         <Text style={styles.temp}>6</Text>
         <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8 </Text>
-          <Text style={styles.highLow}>Low: 6</Text>
-        </View>
+
+        <RowText
+          messageOne={"High: 8"}
+          messsageTwo={"Low: 6"}
+          containerStyles={styles.highLowWrapper}
+          messageOneStyles={styles.highLow}
+          messsageTwoStyles={styles.highLow}
+        />
+        <Text style={styles.highLow}>High: 8 </Text>
+        <Text style={styles.highLow}>Low: 6</Text>
       </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>It's sunny</Text>
-        <Text style={styles.message}>It's perfect t-shirt weather</Text>
-      </View>
+      <RowText
+        messageOne={"It Sunny"}
+        messsageTwo={"It's perfect t-shirt weather"}
+        containerStyles={styles.bodyWrapper}
+        messageOneStyles={styles.description}
+        messsageTwoStyles={styles.message}
+      />
     </SafeAreaView>
   );
 };
