7c92c29599fdc367ba395ac7d74e50a8ec2afd50
Bryium <bryiumonyancha@gmail.com>
2024-10-29 12:18:42 +0300

    add bottom tab navigation

--- App.js
+++ App.js
@@ -4,13 +4,20 @@ import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator
+
+  const Tab = createBottomTabNavigator ()
+
+ } from "@react-navigation/bottom-tabs";
 
 const App = () => {
   return (
     <NavigationContainer>
+      <Tab.Navigator>
       <View style={styles.container}>
         <City />
       </View>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
