09c48e2915b058d59a52c30d486c54ac1931bfd4
Bryium <bryiumonyancha@gmail.com>
2024-10-29 12:46:39 +0300

    remove view and stylesheet

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+
 import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 import CurrentWeather from "./src/screens/CurrentWeather";
@@ -12,18 +12,12 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <View style={styles.container}>
-          <Tab.Screen name={"Current"} component={CurrentWeather} />
-          <Tab.Screen name={"Upcoming"} component={UpcomingWeather} />
-          <Tab.Screen name={"City"} component={City} />
-        </View>
+        <Tab.Screen name={"Current"} component={CurrentWeather} />
+        <Tab.Screen name={"Upcoming"} component={UpcomingWeather} />
+        <Tab.Screen name={"City"} component={City} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+
 export default App;
