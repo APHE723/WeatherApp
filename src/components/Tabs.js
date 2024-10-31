3035493ca2dace7e86a5d694c54bf6d9a5a3bac4
Bryium <bryiumonyancha@gmail.com>
2024-11-01 01:46:07 +0300

    created a new tabs file

---
+++ src/components/Tabs.js
@@ -0,0 +1,7 @@
+import React from "react";
+import UpcomingWeather from "./src/screens/UpcomingWeather";
+import City from "./src/screens/City";
+import CurrentWeather from "./src/screens/CurrentWeather";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
