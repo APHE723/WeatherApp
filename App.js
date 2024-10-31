3825cf20dfc0059eaae8e5d4f3f3cf8a62eac638
Bryium <bryiumonyancha@gmail.com>
2024-11-01 01:28:00 +0300

    add icon to current tab

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from "react";
-
 import UpcomingWeather from "./src/screens/UpcomingWeather";
 import City from "./src/screens/City";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,9 +17,21 @@ const App = () => {
           tabBarActiveTintColor: "grey",
         }}
       >
-        <Tab.Screen name={"Current"} component={CurrentWeather} />
-        <Tab.Screen name={"Upcoming"} component={UpcomingWeather} />
-        <Tab.Screen name={"City"} component={City} />
+        <Tab.Screen
+          name="Current"
+          component={CurrentWeather}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="droplet"
+                size={25}
+                color={focused ? "tomato" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen name="Upcoming" component={UpcomingWeather} />
+        <Tab.Screen name="City" component={City} />
       </Tab.Navigator>
     </NavigationContainer>
   );
