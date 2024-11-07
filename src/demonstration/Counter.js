8d1721f0ed4f97564f5290a67852dad805a8fb18
Bryium <bryiumonyancha@gmail.com>
2024-11-07 23:20:40 +0300

    change the declaration of a variable function

--- src/demonstration/Counter.js
+++ src/demonstration/Counter.js
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
-import { View, Text, Button } from "react-native";
+import React from "react";
+import { View, Text, Button, ImageBackground } from "react-native";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  let count = 0;
 
   return (
-    <View>
-      <Text>{`Count: ${count}`}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{`Count: ${count}`}</Text>
       <Button
         color="red"
         title="Increase the count"
-        onPress={() => setCount(count + 1)}
+        onPress={() => count + 1}
       />
       <Button
         color="green"
         title="Decrease the count"
-        onPress={() => setCount(count - 1)}
+        onPress={() => count - 1}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    ImageBackground: "orange",
+  },
+  title: {
+    alignSelf: "center",
+    fontsize: 25,
+    marginTop: 25,
+  },
+});
 
 export default Counter;
