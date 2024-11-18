4a8c2f156ca742fc33968e43dc13bc3cc176d551
Bryium <bryiumonyancha@gmail.com>
2024-11-18 13:03:59 +0300

    add a weather api key

--- App.js
+++ App.js
@@ -3,11 +3,28 @@ import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 import * as Location from "expo-location";
+import { WEATHER_API_KEY } from "@env";
 
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
   const [error, setError] = useState();
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("Permission to access location was denied");
+        setLoading(false);
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLoading(false);
+    })();
+  }, []);
+
   if (loading) {
     return (
       <View style={styles.container}>
@@ -16,17 +33,6 @@ const App = () => {
     );
   }
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionAync();
-      if (status !== "granted") {
-        setError("Permission to access location was denied");
-        return;
-      }
-      let location = await Location.getCurrentPositionAync({});
-      setLocation(location);
-    })();
-  });
   if (location) {
     console.log(location);
   }
