0076acdb255157f1f94e5273935cb6dd890495a1
Bryium <bryiumonyancha@gmail.com>
2024-12-19 13:00:35 +0300

    styled the View, Text and feather item of the error message that will be displayed

--- src/components/ErrorItem.js
+++ src/components/ErrorItem.js
@@ -4,13 +4,26 @@ import { Feather } from "@expo/vector-icons";
 
 const ErrrorItem = () => {
   return (
-    <View>
-      <Text>Sorry Something went wrong</Text>
+    <View style={styles.container}>
+      <Text style={styles.errormessage}>Sorry Something went wrong</Text>
       <Feather name={"frown"} size={100} color={"white"} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errormessage: {
+    fontSize: 30,
+    color: "white",
+    marginHorizontal: 1,
+    textAlign: "center",
+  },
+});
 
 export default ErrrorItem;
