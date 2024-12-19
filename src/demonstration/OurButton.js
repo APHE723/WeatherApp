0954af2bd90b85b74e1c622f19c01da4551494b9
Bryium <bryiumonyancha@gmail.com>
2024-12-19 13:22:40 +0300

    add the onpress probs

--- src/demonstration/OurButton.js
+++ src/demonstration/OurButton.js
@@ -1,26 +1,30 @@
-import React from "react"
-import  { View, TouchableOpacity, Text} from "react-native"
+import React from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 
 const OurButton = () => {
+  const onPress = () => {
+    console.log("press");
+  };
+
   return (
-    <TouchableOpacity style={styles.container} onPress={}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <Text style={styles.buttonText}>Hello</Text>
     </TouchableOpacity>
-  )
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     elevation: 8,
     backgroundColor: "blue",
     borderRadius: 10,
-    padding: 5
+    padding: 5,
   },
-  buttonText {
+  buttonText: {
     fontSize: 15,
     color: "white",
-    alignSelf: "center"
-  }
-})
-export default OurButton
+    alignSelf: "center",
+  },
+});
+
+export default OurButton;
